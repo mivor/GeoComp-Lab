1cbe73ffabc5a3a7a1e694509b6647bd5ece209a
Ferenc Grecu <mivor20@gmail.com>
2013-11-07 18:46:35 +0200

    New draw logic with Panel and Bitmap

--- GeomComp-Lab1/Form1.Designer.cs
+++ GeomComp-Lab1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
+            this.PictureBox = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // btnStart
@@ -41,15 +42,24 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // PictureBox
+            // 
+            this.PictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PictureBox.Location = new System.Drawing.Point(13, 27);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(259, 222);
+            this.PictureBox.TabIndex = 1;
+            this.PictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_Paint);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.PictureBox);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
@@ -57,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel PictureBox;
     }
 }
 
--- GeomComp-Lab1/Form1.cs
+++ GeomComp-Lab1/Form1.cs
@@ -13,26 +13,40 @@ namespace GeomComp_Lab1
     public partial class Form1 : Form
     {
         PointGenerator pointMaker;
-        Graphics formGraph;
-        Point min = new Point(50, 50);
+        Bitmap bitmap;
+        Point min = new Point(0, 0);
         Point max = new Point(250, 250);
 
         public Form1()
         {
             InitializeComponent();
             CenterToScreen();
-            formGraph = this.CreateGraphics();
-            pointMaker = new PointGenerator(min, max, 20, formGraph);
+            PictureBox.Width = max.X + 30;
+            PictureBox.Height = max.Y + 30;
+            bitmap = new Bitmap(PictureBox.Width, PictureBox.Height);
+            using (Graphics gx = Graphics.FromImage(bitmap))
+            {
+                pointMaker = new PointGenerator(min, max, 20, gx);
+                pointMaker.DrawPoints();
+            }
+            
+            this.Width = PictureBox.Width + 40;
+            this.Height = PictureBox.Height + 80;
+            PictureBox.Invalidate();
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            Program.MinimumAreaRectangleSimple(formGraph, pointMaker);
+            using (Graphics gx = Graphics.FromImage(bitmap))
+            {
+                Program.MinimumAreaRectangleSimple(gx, pointMaker);
+            }
+            PictureBox.Invalidate();
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
         {
-            pointMaker.DrawPoints();
+            e.Graphics.DrawImage(bitmap, 0, 0);
         }
     }
 }
